3894ee06dd9334b6c8a1d754de56f9e9d0b5dc66
Ivan S <Zridq@yandex.ru>
2024-03-03 12:21:38 +0300

    sixth step

--- Makefile
+++ Makefile
@@ -5,13 +5,13 @@ build:
 	@poetry build
 
 publish:
-	@poetry publish --dry-run
+	@poetry publish --dry-run --username BIBA --password BUBA
 
 package-install:
 	@python3 -m pip install --user dist/*.whl
 
 RE:
 	@python3 -m pip install --force-reinstall --user dist/*.whl
-
+ 
 lint:
 	@poetry run flake8 brain_games
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,24 +1,28 @@
 from random import randint
+import random
+import operator
 from brain_games.engine import comparing
 from brain_games.cli import welcome_user
 
 
-def brain_even():
-    question = randint(1, 100)
-    if question % 2 == 0:
-        result = 'yes'
-    else:
-        result = 'no'
+def brain_calc():
+    number_a = randint(1, 100)
+    number_b = randint(1, 100)
+    operators = [('+', operator.add), ('-', operator.sub), ('*', operator.mul)]
+    op, fn = random.choice(operators)
+    result = fn(number_a, number_b)
+    question = [str(number_a), str(op), str(number_b)]
+    question = " ".join(question)
     return question, result
 
 
 def repeatting():
     counter = 0
-    sending = brain_even()
+    sending = brain_calc()
     name = welcome_user()
     counter = comparing(sending[0], sending[1], name, counter)
     while counter < 3:
-        sending = brain_even()
+        sending = brain_calc()
         counter = comparing(sending[0], sending[1], name, counter)
     if counter == 3:
         print('Congratulations, ' + name + '!')
